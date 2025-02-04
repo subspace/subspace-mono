eb2daee79ace114972b1db14bfd63c39296a9441
teor <teor@riseup.net>
2025-02-04 13:00:24 +1000

    Taurus: bump pallet-domains storage version & runtime spec version

--- crates/subspace-runtime/src/lib.rs
+++ crates/subspace-runtime/src/lib.rs
@@ -121,7 +121,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("subspace"),
     impl_name: Cow::Borrowed("subspace"),
     authoring_version: 0,
-    spec_version: 11,
+    spec_version: 12,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 0,
